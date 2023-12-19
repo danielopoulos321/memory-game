a4d758dab0cee50047ea2389de63dbaedcedfada
Daniel Anestopoulos <dstops97@gmail.com>
2023-12-19 14:06:30 -0500

    Refactor CardComponent + Add Restart Logic

--- src/GameMaster.jsx
+++ src/GameMaster.jsx
@@ -1,19 +1,37 @@
 import './styles/GameMaster.css';
 import { useState, useEffect } from 'react';
 import { fetchData, arrayRandomizer } from './helperFunctions';
-import Card from './components/Card';
+import CardContainer from './components/Cards';
+import Header from './components/Header';
+import Modal from './components/Modal';
 
 export default function GameMaster() {
   const [pokemon, setPokemon] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [isDialogOpen, setDialogOpen] = useState(false);
   const [shouldShuffle, setShouldShuffle] = useState(false);
+  const [shouldRestart, setShouldRestart] = useState(false);
 
   const handleClick = (clickedPokemon) => {
+    const validClick = pokemon.some(
+      (pokemon) => pokemon.id == clickedPokemon.id && pokemon.clicked == false,
+    );
+    if (validClick) {
+      setScore(score + 1);
+    } else {
+      if (score > highScore) {
+        setHighScore(score);
+      }
+      setScore(0);
+      setDialogOpen(true);
+    }
+
     setPokemon((prevPokemon) =>
       prevPokemon.map((p) =>
         p.id === clickedPokemon.id ? { ...p, clicked: true } : p,
       ),
     );
-    console.log(clickedPokemon.id);
     setShouldShuffle(true);
   };
 
@@ -26,28 +44,18 @@ export default function GameMaster() {
 
   useEffect(() => {
     fetchData(setPokemon);
-  }, []);
+    setShouldRestart(false);
+  }, [shouldRestart]);
 
   return (
     <>
-      {pokemon.map((pokemon) => {
-        return (
-          <Card
-            onClick={() => handleClick(pokemon)}
-            key={pokemon.id}
-            pokemon={pokemon}
-          />
-        );
-      })}
+      <Header score={score} highScore={highScore} />
+      <CardContainer pokemon={pokemon} handleClick={handleClick} />
+      <Modal
+        isDialogOpen={isDialogOpen}
+        setDialogOpen={setDialogOpen}
+        setShouldRestart={setShouldRestart}
+      />
     </>
   );
 }
-
-//Current Score + Best Score as states in Game Master
-//  Passed to header to update
-
-//FILE STRUCTURE
-//GameMaster
-//COMPONENTS
-//Card
-//Header(with Score)
