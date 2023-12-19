7a4bd661f35075c72768464a508c62ade29c1c5f
Daniel Anestopoulos <dstops97@gmail.com>
2023-12-18 22:56:10 -0500

    Add effect to store images in state

--- src/GameMaster.jsx
+++ src/GameMaster.jsx
@@ -1,20 +1,24 @@
 import './styles/GameMaster.css';
+import { useState, useEffect } from 'react';
+import { fetchData } from './helperFunctions';
 
-function GameMaster() {}
+export default function GameMaster() {
+  const [pokemon, setPokemon] = useState([]);
 
-export default GameMaster;
+  useEffect(() => {
+    fetchData(setPokemon);
+  }, []);
+}
 
 //Current Score + Best Score as states in Game Master
 //Onload random assortment of Pokemon objects stored in state
-//Random Set of #'s between 1-150
-//Return image by inserting number into Api call
+
 //Generate random number 1/100 chance of pulling shiny image
-//{ imageUrl, clicked? }
-//useEffect to pull all images onMount only
-//Images get mapped to Card components
-//Each card click adds that object to a clicked state array
-//Master Pokemon array state is randomized
-//Trigger-rerendering
+
+//  Images get mapped to Card components
+//      Each card click adds that object to a clicked state array
+//          Master Pokemon array state is randomized
+//          Trigger-rerendering
 
 //FILE STRUCTURE
 //GameMaster
