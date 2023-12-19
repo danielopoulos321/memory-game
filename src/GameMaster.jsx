b4d46d7b38f6cb9dd5b3f99ac21ad09b2ca9bebf
Daniel Anestopoulos <dstops97@gmail.com>
2023-12-19 16:51:02 -0500

    Reset pokemon on restart

--- src/GameMaster.jsx
+++ src/GameMaster.jsx
@@ -43,6 +43,7 @@ export default function GameMaster() {
   }, [shouldShuffle]);
 
   useEffect(() => {
+    setPokemon([]);
     fetchData(setPokemon);
     setShouldRestart(false);
   }, [shouldRestart]);
