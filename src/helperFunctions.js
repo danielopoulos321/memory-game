88963124b09048f1b5b29ce75dcd0dde49497b3b
Daniel Anestopoulos <dstops97@gmail.com>
2023-12-19 11:02:07 -0500

    Add arrayRandomizer

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -30,3 +30,14 @@ export async function fetchData(setPokemon) {
     console.error('Error fetching data:', error);
   }
 }
+
+export function arrayRandomizer(originalArray) {
+  const newArray = [...originalArray];
+
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+
+  return newArray;
+}
